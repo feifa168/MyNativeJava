d341435caf1447606c2e5aef8cbbbe7d8ae37c07
feifa168 <2900093533@qq.com>
2018-11-30 17:44:36 +0800

    增加测试接口，本次实现以下几个接口，主要测试各种类型在jni中的转换。 Java_com_ft_mynative_MyNativeJava_baseTypes用于测试java基本数据类型 Java_com_ft_mynative_MyNativeJava_wrapBaseTypes用于测试java封装8种基本数据类型的类(Integer...) Java_com_ft_mynative_MyNativeJava_intArray用于测试int[]数组的转换

--- src/main/java/com/ft/mynative/MyNativeJava.java
+++ src/main/java/com/ft/mynative/MyNativeJava.java
@@ -5,14 +5,67 @@ public class MyNativeJava {
         System.loadLibrary("libMyNativeJava");
     }
 
+    private int num = 5;
+    private char[] ca = {5, 3};
+    private String str = "tom";
+    private String[] strA = {"one", "two"};
+
+    private static int snum = 5;
+    private static char[] sca = {5, 3};
+    private static String sstr = "bruce";
+    private static String[] sstrA = {"Monday", "Tuesty"};
+
+    private void testInt(int a) {
+        System.out.println("testInt(int " + a + ")");
+    }
+    private void testString(String s) {
+        System.out.println("testString(String " + s + ")");
+    }
+
+    private static void stestInt(int a) {
+        System.out.println("testInt(int " + a + ")");
+    }
+    private static void stestString(String s) {
+        System.out.println("testString(String " + s + ")");
+    }
+
     private native void fun();
     private native int fun2(String s);
     private native void fun3(Integer[] ia);
 
+    private native boolean baseTypes(int intA, char charB, boolean boolC, short shortD, long longE, float floatF, double doubleG, byte byteH);
+    private native Boolean wrapBaseTypes(Integer intA, Character charB, Boolean boolC6, Short shortD, Long longE, Float floatF, Double doubleG, Byte byteH);
+    private native boolean[] intArray(int[] iarr);
+    private native Boolean[] wrapIntArray(Integer[] iarr);
+    private native String stringTest(String s);
+    private native String[] strArrTest(String[] sa);
+    private native void modifyFields();
+    private native void testMethod();
+
     public static void main(String[] args) {
-        MyNativeJava nativeHello = new MyNativeJava();
-        nativeHello.fun();
-        System.out.println(nativeHello.fun2("hello, tom"));
-        nativeHello.fun3(new Integer[]{3, 5});
+        MyNativeJava nativeJava = new MyNativeJava();
+//        nativeJava.fun();
+//        System.out.println(nativeJava.fun2("hello, tom"));
+//        nativeJava.fun3(new Integer[]{3, 5});
+
+        nativeJava.baseTypes(4, 'a', true, (short)8, 100, 1.5f, 2.79, (byte)57);
+        new Integer(5).intValue();
+        new Boolean(false).booleanValue();
+        new Short((short)4).shortValue();
+        new Long((long)6).longValue();
+        new Character('a').charValue();
+        new Float(5.0f).floatValue();
+        new Double(5.9).doubleValue();
+        new Byte((byte)5).byteValue();
+        Boolean.valueOf(false);
+        Boolean b = nativeJava.wrapBaseTypes(Integer.valueOf(5), Character.valueOf('a'), Boolean.valueOf(true),
+                Short.valueOf((short)6), Long.valueOf((long)800), Float.valueOf(1.2f), Double.valueOf(3.4), Byte.valueOf((byte)'a'));
+        System.out.println(b);
+
+        int ia[] = {1, 2, 3, 4, 5, 6};
+        boolean[] ba = nativeJava.intArray(ia);
+        for (int i=0; i<ba.length; i++) {
+            System.out.println("num is " + ia[i] + ", &1=" + ba[i]);
+        }
     }
 }
--- src/main/java/include/com_ft_mynative_MyNativeJava.h
+++ src/main/java/include/com_ft_mynative_MyNativeJava.h
@@ -31,6 +31,70 @@ JNIEXPORT jint JNICALL Java_com_ft_mynative_MyNativeJava_fun2
 JNIEXPORT void JNICALL Java_com_ft_mynative_MyNativeJava_fun3
   (JNIEnv *, jobject, jobjectArray);
 
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    baseTypes
+ * Signature: (ICZSJFDB)Z
+ */
+JNIEXPORT jboolean JNICALL Java_com_ft_mynative_MyNativeJava_baseTypes
+  (JNIEnv *, jobject, jint, jchar, jboolean, jshort, jlong, jfloat, jdouble, jbyte);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    wrapBaseTypes
+ * Signature: (Ljava/lang/Integer;Ljava/lang/Character;Ljava/lang/Boolean;Ljava/lang/Short;Ljava/lang/Long;Ljava/lang/Float;Ljava/lang/Double;Ljava/lang/Byte;)Ljava/lang/Boolean;
+ */
+JNIEXPORT jobject JNICALL Java_com_ft_mynative_MyNativeJava_wrapBaseTypes
+  (JNIEnv *, jobject, jobject, jobject, jobject, jobject, jobject, jobject, jobject, jobject);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    intArray
+ * Signature: ([I)[Z
+ */
+JNIEXPORT jbooleanArray JNICALL Java_com_ft_mynative_MyNativeJava_intArray
+  (JNIEnv *, jobject, jintArray);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    wrapIntArray
+ * Signature: ([Ljava/lang/Integer;)[Ljava/lang/Boolean;
+ */
+JNIEXPORT jobjectArray JNICALL Java_com_ft_mynative_MyNativeJava_wrapIntArray
+  (JNIEnv *, jobject, jobjectArray);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    stringTest
+ * Signature: (Ljava/lang/String;)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_com_ft_mynative_MyNativeJava_stringTest
+  (JNIEnv *, jobject, jstring);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    strArrTest
+ * Signature: ([Ljava/lang/String;)[Ljava/lang/String;
+ */
+JNIEXPORT jobjectArray JNICALL Java_com_ft_mynative_MyNativeJava_strArrTest
+  (JNIEnv *, jobject, jobjectArray);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    modifyFields
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_ft_mynative_MyNativeJava_modifyFields
+  (JNIEnv *, jobject);
+
+/*
+ * Class:     com_ft_mynative_MyNativeJava
+ * Method:    testMethod
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_ft_mynative_MyNativeJava_testMethod
+  (JNIEnv *, jobject);
+
 #ifdef __cplusplus
 }
 #endif
